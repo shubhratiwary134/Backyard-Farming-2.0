fa23fa00a52a047f14b82b39bac690affc2d3ba2
shubhra tiwary <shubhratiwary5@gmail.com>
2024-09-19 22:51:05 +0530

    backend file structure done and server established

---
+++ Api/src/index.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+app.listen(port, () => {
+  console.log("the server is running now");
+});
