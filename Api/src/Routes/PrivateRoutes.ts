ea771a741fd3809a51b9e2da91334f56e50bd23e
shubhra tiwary <shubhratiwary5@gmail.com>
2024-09-21 19:13:14 +0530

    clerkConfig

--- Api/src/Routes/PrivateRoutes.ts
+++ Api/src/Routes/PrivateRoutes.ts
@@ -5,10 +5,7 @@ const express = require("express");
 
 const privateRouter = express.Router();
 
-const clerkConfig = {
-  apiKey: process.env.CLERK_SECRET_KEY,
-  apiVersion: 2,
-};
+
 
 // we first check the authentication for the private routes
 privateRouter.use(ClerkExpressWithAuth());
