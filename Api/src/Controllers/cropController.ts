3847d78b45a5243649006fc6a51b7d71bb5e5d52
shubhra tiwary <shubhratiwary5@gmail.com>
2024-09-22 07:22:28 +0530

    completed the getfamous endpoint

--- Api/src/Controllers/cropController.ts
+++ Api/src/Controllers/cropController.ts
@@ -1,1 +1,8 @@
-export const getFamousCrops = () => {};
+import { Request, Response } from "express";
+
+const Plantorium = require("../Models/PlantoriumModel");
+export const getFamousCrops = async (req: Request, res: Response) => {
+  const famousCrops = await Plantorium.find({ famous: true });
+
+  return res.status(200).send(famousCrops);
+};
