60bbd45860dc01f9458bbf220f5509942e4cefc0
shubhra tiwary <shubhratiwary5@gmail.com>
2024-09-22 07:11:40 +0530

    create Plantorium functionality completed

--- Api/src/Controllers/PlantoriumController.ts
+++ Api/src/Controllers/PlantoriumController.ts
@@ -1,3 +1,45 @@
-export const createPlantorium = () => {
-  console.log("creating plantorium");
+import { Request, Response } from "express";
+const Plantorium = require("../Models/PlantoriumModel");
+export const createPlantorium = async (req: Request, res: Response) => {
+  const data = req.body;
+  if (!data) {
+    return res
+      .status(400)
+      .json({ message: "Cannot create Plantorium: missing Fields " });
+  }
+  const {
+    userId,
+    averageRainfall,
+    soilType,
+    soilColor,
+    soilTexture,
+    soilPH,
+    pastCrops,
+    cropDiseases,
+    affectedCrops,
+    waterSupply,
+    landArea,
+    famous,
+    createdAt,
+  } = req.body;
+  try {
+    await Plantorium.create({
+      userId,
+      averageRainfall,
+      soilType,
+      soilColor,
+      soilTexture,
+      soilPH,
+      pastCrops,
+      cropDiseases,
+      affectedCrops,
+      waterSupply,
+      landArea,
+      famous,
+      createdAt,
+    });
+    res.status(201).json({ message: "plantorium successfully created" });
+  } catch (err) {
+    console.log(err);
+  }
 };
