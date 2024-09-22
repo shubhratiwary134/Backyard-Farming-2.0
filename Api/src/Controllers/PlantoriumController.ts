47ebe6e7547c59641e33b7c84c3032ff9d098450
shubhra tiwary <shubhratiwary5@gmail.com>
2024-09-22 14:14:22 +0530

    getspecificPlantorium route created

--- Api/src/Controllers/PlantoriumController.ts
+++ Api/src/Controllers/PlantoriumController.ts
@@ -43,3 +43,16 @@ export const createPlantorium = async (req: Request, res: Response) => {
     console.log(err);
   }
 };
+
+export const getSpecificPlantorium = async (req: Request, res: Response) => {
+  const plantoriumId = req.params.id;
+  try {
+    const plantorium = await Plantorium.findById(plantoriumId);
+    if (!plantorium) {
+      return res.status(404).json({ message: "Plantorium not found" });
+    }
+    return res.status(200).json(plantorium);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching plantorium", err });
+  }
+};
--- Api/src/Routes/plantoriumRoutes.ts
+++ Api/src/Routes/plantoriumRoutes.ts
@@ -1,8 +1,11 @@
-import { createPlantorium } from "../Controllers/PlantoriumController";
+import {
+  createPlantorium,
+  getSpecificPlantorium,
+} from "../Controllers/PlantoriumController";
 const express = require("express");
 
 const plantoriumRoutes = express.Router();
 
 plantoriumRoutes.post("/createNew", createPlantorium);
-
+plantoriumRoutes.get("/:id", getSpecificPlantorium);
 export default plantoriumRoutes;
