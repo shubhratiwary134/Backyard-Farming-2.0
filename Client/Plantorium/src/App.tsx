ea9369cf838f866bec2dfb6e9620c3756728b095
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-07 20:25:41 +0530

    Navbar component added

--- Client/Plantorium/src/App.tsx
+++ Client/Plantorium/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <>whatever</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
