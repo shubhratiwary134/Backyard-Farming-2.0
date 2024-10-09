d980f8675416098b96bcb4f401d63df78184c1fa
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-09 21:30:47 +0530

    redux toolkit installed

--- Client/Plantorium/src/App.tsx
+++ Client/Plantorium/src/App.tsx
@@ -2,10 +2,12 @@ import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import LandingPage from "./Pages/LandingPage";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <SignedIn>
         <HomePage />
       </SignedIn>
