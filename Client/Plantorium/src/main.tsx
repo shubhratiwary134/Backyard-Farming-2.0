524b0ee3912cf26716e3345d726d28d50636b450
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-11 19:05:39 +0530

    wrapped the provider in the main.tsx

--- Client/Plantorium/src/main.tsx
+++ Client/Plantorium/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { store } from "./store/store.ts";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
@@ -10,8 +12,10 @@ if (!PUBLISHABLE_KEY) {
 }
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
-    </ClerkProvider>
+    <Provider store={store}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
+    </Provider>
   </StrictMode>
 );
