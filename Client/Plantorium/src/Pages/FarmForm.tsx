ea86e7d4882e5a5fa9dce010f0ea9e54be4f16ce
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-20 13:20:39 +0530

    changed the placeholder

--- Client/Plantorium/src/Pages/FarmForm.tsx
+++ Client/Plantorium/src/Pages/FarmForm.tsx
@@ -4,16 +4,16 @@ import plantoriumValidationSchema from "../Schema/FarmSchema";
 const FarmForm = () => {
   const formik = useFormik({
     initialValues: {
-      averageRainfall: "", // Average rainfall as a number
-      soilType: "", // Soil type as a string
-      soilColor: "", // Soil color as a string
-      soilTexture: "", // Soil texture as a string
-      soilPH: "", // Soil pH as a number
-      pastCrops: [""], // Past crops as an array of strings
-      cropDiseases: [""], // Crop diseases as an array of strings
-      affectedCrops: [""], // Affected crops as an array of strings
-      waterSupply: "", // Water supply as a string
-      landArea: "", // Land area as a number
+      averageRainfall: "",
+      soilType: "",
+      soilColor: "",
+      soilTexture: "",
+      soilPH: "",
+      pastCrops: [""],
+      cropDiseases: [""],
+      affectedCrops: [""],
+      waterSupply: "",
+      landArea: "",
     },
     validationSchema: plantoriumValidationSchema,
     onSubmit: (values) => {
@@ -23,92 +23,204 @@ const FarmForm = () => {
 
   return (
     <>
-      <div>
-        <form autoComplete="off">
-          <div className="flex flex-col">
-            <label htmlFor="AverageRainfall"> Rainfall</label>
+      <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <form autoComplete="off" onSubmit={formik.handleSubmit}>
+          {/* Average Rainfall */}
+          <div className="mb-4">
+            <label
+              htmlFor="averageRainfall"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Average Rainfall (mm)
+            </label>
             <input
               value={formik.values.averageRainfall}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               type="text"
-              placeholder="enter average rainfall"
+              id="averageRainfall"
+              placeholder="Enter average rainfall"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
 
-          <label htmlFor="AverageRainfall">Soil Type</label>
-          <input
-            value={formik.values.soilType}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Soil COlor</label>
-          <input
-            value={formik.values.soilColor}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">soil texture</label>
-          <input
-            value={formik.values.soilTexture}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">soil ph</label>
-          <input
-            value={formik.values.soilPH}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Past crops</label>
-          <input
-            value={formik.values.pastCrops}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Crop Diseases</label>
-          <input
-            value={formik.values.cropDiseases}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Affected Crops</label>
-          <input
-            value={formik.values.affectedCrops}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Water Supply</label>
-          <input
-            value={formik.values.waterSupply}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <label htmlFor="AverageRainfall">Land Area</label>
-          <input
-            value={formik.values.landArea}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            type="text"
-            placeholder="enter average rainfall"
-          ></input>
-          <button type="submit">submit</button>
+          {/* Soil Type */}
+          <div className="mb-4">
+            <label
+              htmlFor="soilType"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Soil Type
+            </label>
+            <input
+              value={formik.values.soilType}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="soilType"
+              placeholder="Enter soil type"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Soil Color */}
+          <div className="mb-4">
+            <label
+              htmlFor="soilColor"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Soil Color
+            </label>
+            <input
+              value={formik.values.soilColor}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="soilColor"
+              placeholder="Enter soil color"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Soil Texture */}
+          <div className="mb-4">
+            <label
+              htmlFor="soilTexture"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Soil Texture
+            </label>
+            <input
+              value={formik.values.soilTexture}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="soilTexture"
+              placeholder="Enter soil texture"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Soil pH */}
+          <div className="mb-4">
+            <label
+              htmlFor="soilPH"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Soil pH
+            </label>
+            <input
+              value={formik.values.soilPH}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="soilPH"
+              placeholder="Enter soil pH"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Past Crops */}
+          <div className="mb-4">
+            <label
+              htmlFor="pastCrops"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Past Crops
+            </label>
+            <input
+              value={formik.values.pastCrops}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="pastCrops"
+              placeholder="Enter past crops"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Crop Diseases */}
+          <div className="mb-4">
+            <label
+              htmlFor="cropDiseases"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Crop Diseases
+            </label>
+            <input
+              value={formik.values.cropDiseases}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="cropDiseases"
+              placeholder="Enter crop diseases"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Affected Crops */}
+          <div className="mb-4">
+            <label
+              htmlFor="affectedCrops"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Affected Crops
+            </label>
+            <input
+              value={formik.values.affectedCrops}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="affectedCrops"
+              placeholder="Enter affected crops"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Water Supply */}
+          <div className="mb-4">
+            <label
+              htmlFor="waterSupply"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Water Supply
+            </label>
+            <input
+              value={formik.values.waterSupply}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="waterSupply"
+              placeholder="Enter water supply"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Land Area */}
+          <div className="mb-4">
+            <label
+              htmlFor="landArea"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Land Area (in acres)
+            </label>
+            <input
+              value={formik.values.landArea}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              type="text"
+              id="landArea"
+              placeholder="Enter land area"
+              className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </>
