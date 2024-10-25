c30b4fc4009363096671dae4c219a23b3afedaa1
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-25 16:42:24 +0530

    form updated

--- Client/Plantorium/src/Pages/FarmForm.tsx
+++ Client/Plantorium/src/Pages/FarmForm.tsx
@@ -5,16 +5,24 @@ const FarmForm = () => {
     initialValues: {
       averageRainfall: 0,
       soilType: "",
+      soilColor: "",
+      soilTexture: "",
+      soilPh: 0,
+      waterSupply: "",
+      landArea: 0,
     },
     onSubmit: (values) => console.log(values),
   });
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="ml-10 mt-10 flex flex-col gap-10 w-1/4"
+      >
         <input
           type="number"
           name="averageRainfall"
-          placeholder="enter the Average Rainfall"
+          placeholder="Enter The Average Rainfall"
           value={values.averageRainfall}
           onChange={handleChange}
         />
@@ -30,6 +38,52 @@ const FarmForm = () => {
           <option value="Sandy" label="Sandy" />
           <option value="Alluvial" label="Alluvial" />
         </select>
+        <select
+          id="soilColor"
+          name="soilColor"
+          value={values.soilColor}
+          onChange={handleChange}
+        >
+          <option value="" label="Select soil Color" />
+          <option value="Black" label="Black" />
+          <option value="Brownish" label="Brownish" />
+        </select>
+        <select
+          id="soilTexture"
+          name="soilTexture"
+          value={values.soilTexture}
+          onChange={handleChange}
+        >
+          <option value="" label="Select Soil Texture" />
+          <option value="Soft" label="Soft" />
+          <option value="Hard" label="Hard" />
+          <option value="Airy" label="Airy" />
+        </select>
+        <input
+          type="number"
+          name="soilPh"
+          placeholder="Enter the soilPh"
+          value={values.soilPh}
+          onChange={handleChange}
+        />
+        <select
+          id="waterSupply"
+          name="waterSupply"
+          value={values.waterSupply}
+          onChange={handleChange}
+        >
+          <option value="" label="Select Water Supply" />
+          <option value="Channel" label="Channel" />
+          <option value="Tube Well" label="Tube Well" />
+          <option value="Other" label="Other" />
+        </select>
+        <input
+          type="number"
+          name="landArea"
+          placeholder="Enter the landArea"
+          value={values.landArea}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
     </>
