28f75105806e8bfbbca182dc80bc4fd085ef88da
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-26 19:51:10 +0530

    navbar boiler code

--- Client/Plantorium/src/Components/Navbar.tsx
+++ Client/Plantorium/src/Components/Navbar.tsx
@@ -1,11 +1,17 @@
 const Navbar = () => {
   return (
-    <div className="px-5 flex justify-between">
+    <div className="p-5 flex justify-between items-start">
       <div className="">Backyard Farming 2.O</div>
-      <div className="flex justify-around w-1/4">
-        <a href="#">Help</a>
-        <a href="#">Community</a>
-        <a href="#">Weekly Reports</a>
+      <div className="flex justify-around w-1/3">
+        <a href="#">How It Works</a>
+        <a href="#">Plans</a>
+        <a href="#">FAQS</a>
+        <div className="flex gap-10 ">
+          <button className="bg-green-600 px-5 py-2 rounded-full">LogIn</button>
+          <button className="bg-green-600 px-5 py-2 rounded-full">
+            SignUp
+          </button>
+        </div>
       </div>
     </div>
   );
