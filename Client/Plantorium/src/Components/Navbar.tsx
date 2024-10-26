e83f387ff2bbf92aff444bae72632c416588ea29
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-26 19:59:06 +0530

    padding changed

--- Client/Plantorium/src/Components/Navbar.tsx
+++ Client/Plantorium/src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="p-5 flex justify-between items-center">
+    <div className="px-5 py-2 flex justify-between ">
       <div className="">Backyard Farming 2.O</div>
       <div className="flex justify-around items-center w-1/3">
         <a href="#">How It Works</a>
