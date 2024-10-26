bf21c55bd76974c8399ae3e7f741289ab7f535ad
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-26 19:54:49 +0530

    fixed positioning

--- Client/Plantorium/src/Components/Navbar.tsx
+++ Client/Plantorium/src/Components/Navbar.tsx
@@ -1,14 +1,16 @@
 const Navbar = () => {
   return (
-    <div className="p-5 flex justify-between items-start">
+    <div className="p-5 flex justify-between items-center">
       <div className="">Backyard Farming 2.O</div>
-      <div className="flex justify-around w-1/3">
+      <div className="flex justify-around items-center w-1/3">
         <a href="#">How It Works</a>
         <a href="#">Plans</a>
         <a href="#">FAQS</a>
         <div className="flex gap-10 ">
-          <button className="bg-green-600 px-5 py-2 rounded-full">LogIn</button>
-          <button className="bg-green-600 px-5 py-2 rounded-full">
+          <button className="bg-green-600 text-white px-5 py-2 rounded-full">
+            LogIn
+          </button>
+          <button className="bg-green-600 text-white px-5 py-2 rounded-full">
             SignUp
           </button>
         </div>
