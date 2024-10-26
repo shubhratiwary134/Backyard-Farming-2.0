8d287e82e3baedaca87e6abd6f1fb4deab65c137
shubhra tiwary <shubhratiwary5@gmail.com>
2024-10-26 19:41:27 +0530

    Navbar created

--- Client/Plantorium/src/Components/Navbar.tsx
+++ Client/Plantorium/src/Components/Navbar.tsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="px-5 flex justify-between">
+      <div className="">Backyard Farming 2.O</div>
+      <div className="flex justify-around w-1/4">
+        <a href="#">Help</a>
+        <a href="#">Community</a>
+        <a href="#">Weekly Reports</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
