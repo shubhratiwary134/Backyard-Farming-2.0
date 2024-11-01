68b36018717ab826df27923672fe7760f669d34f
shubhra tiwary <shubhratiwary5@gmail.com>
2024-11-01 22:02:29 +0530

    navbar anchor tag updation

--- Client/Plantorium/src/Components/Navbar.tsx
+++ Client/Plantorium/src/Components/Navbar.tsx
@@ -11,14 +11,11 @@ const Navbar = () => {
     <div className="px-5 py-2 flex justify-between ">
       <div className="">Backyard Farming 2.O</div>
       <div className="flex justify-around items-center w-1/3">
-        <a href="#">How It Works</a>
-        <a href="#">Plans</a>
-        <a href="#">Team</a>
-        <a href="#">FAQS</a>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
         <SignedOut>
+          <a href="#">How It Works</a>
+          <a href="#">Plans</a>
+          <a href="#">Team</a>
+          <a href="#">FAQS</a>
           <div className="flex gap-5 ">
             <SignUpButton>
               <button className="bg-[#66f542] text-black px-5 py-2 rounded-full">
@@ -32,6 +29,13 @@ const Navbar = () => {
             </SignInButton>
           </div>
         </SignedOut>
+        <SignedIn>
+          <a href="#">Start A Farm</a>
+          <a href="#">View Progress</a>
+          <a href="#">Team</a>
+          <a href="#">FAQS</a>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
