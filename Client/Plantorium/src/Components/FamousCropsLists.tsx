bf8755c46df8727b6d3ad055631698ec0605e982
shubhra tiwary <shubhratiwary5@gmail.com>
2024-11-01 22:44:05 +0530

    famous Crops component layout implemented

--- Client/Plantorium/src/Components/FamousCropsLists.tsx
+++ Client/Plantorium/src/Components/FamousCropsLists.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/Hook";
 import { getFamousCrops } from "../store/thunks/cropThunk";
+import image1 from "../Assests/YellowTheme.jpg";
 
 const FamousCropsLists = () => {
   const dispatch = useAppDispatch();
@@ -12,9 +13,12 @@ const FamousCropsLists = () => {
     <div className="grid grid-cols-3 gap-20">
       {allFamousCrops.map((crop) => {
         return (
-          <div className="w-96 border-2 p-10 border-black">
-            <div>{crop.name}</div>
-            <div>{crop.additionalTips}</div>
+          <div className="w-96  flex flex-col border-2 gap-5 border-gray-200 rounded-lg">
+            <img src={image1} className="w-full h-2/3" />
+            <div className="text-xl mr-5 ml-2">{crop.name}</div>
+            <div className="text-md mr-5 ml-2 text-neutral-900">
+              {crop.additionalTips}
+            </div>
           </div>
         );
       })}
