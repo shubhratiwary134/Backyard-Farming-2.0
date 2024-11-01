ddd8b1af92dcac9a6ebcf1da93f48e5a019aeb81
shubhra tiwary <shubhratiwary5@gmail.com>
2024-11-01 13:16:51 +0530

    irrelevant divs removed

--- Client/Plantorium/src/Components/LandingPageComponents/HowDoesItWork.tsx
+++ Client/Plantorium/src/Components/LandingPageComponents/HowDoesItWork.tsx
@@ -9,30 +9,25 @@ const HowDoesItWork = () => {
         AI analyzes your unique backyard farm and provides personalized guidance
       </div>
       <div className="flex w-full justify-between mt-5">
-        <div className="w-96 pb-5 flex flex-col border-2 gap-20 border-gray-200 rounded-lg">
-          <div>
-            <img src={image1} className="w-full rounded-lg" />
-          </div>
-          <div className="text-wrap">
+        <div className="w-96  flex flex-col border-2 gap-20 border-gray-200 rounded-lg">
+          <img src={image1} className="w-full rounded-lg h-2/3" />
+          <div className="text-sm">
             Start by uploading pictures of your plants or the crops you want to
             grow. Don’t have photos? No problem! Select from a list of popular
             crops to get started.
           </div>
         </div>
         <div className="w-96  flex flex-col border-2 border-gray-200 rounded-lg">
-          <div>
-            <img src={image2} className="w-full rounded-lg" />
-          </div>
-          <div>
+          <img src={image2} className="w-full rounded-lg h-2/3" />
+
+          <div className="text-sm">
             Based on your plant choice, we’ll create a tailored weekly roadmap
             with all the steps you need to help your plants thrive.
           </div>
         </div>
         <div className="w-96  flex flex-col border-2 border-gray-200 rounded-lg">
-          <div>
-            <img src={image3} className=" rounded-lg" />
-          </div>
-          <div>
+          <img src={image3} className="w-full rounded-lg h-2/3" />
+          <div className="text-sm">
             Check off completed tasks each week, track your progress, and share
             tips and experiences with other backyard gardeners
           </div>
