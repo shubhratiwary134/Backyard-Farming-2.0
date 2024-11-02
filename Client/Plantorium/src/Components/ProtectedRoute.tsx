e9de80e5a4a4cd8c74e206df6cd2831f7bcd7091
shubhra tiwary <shubhratiwary5@gmail.com>
2024-11-02 17:32:55 +0530

    updated the protected route logic by using the isLoaded state from useAuth

--- Client/Plantorium/src/Components/ProtectedRoute.tsx
+++ Client/Plantorium/src/Components/ProtectedRoute.tsx
@@ -1,11 +1,26 @@
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from "react-router";
+import { useEffect, useState } from "react";
 
 const ProtectedRoute = ({ children }) => {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth(); //added the isLoaded
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (isLoaded) {
+      setLoading(false);
+    }
+  }, [isLoaded]);
+
+  // Show a loading indicator or fallback component while checking auth status
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   if (!isSignedIn) {
     return <Navigate to="/sign-in" />;
   }
+
   return children;
 };
 
