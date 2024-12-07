5899bdf11c7052a346e4949bd9a2b63273e2b275
shubhra tiwary <shubhratiwary5@gmail.com>
2024-12-07 21:54:23 +0530

    minor fixes

--- Client/Plantorium/src/store/thunks/plantoriumThunk.ts
+++ Client/Plantorium/src/store/thunks/plantoriumThunk.ts
@@ -7,15 +7,18 @@ export const createAFarm = createAsyncThunk(
     try {
       const response = await axios.post(
         "http://localhost:3000/plantorium/createNew",
+        {},
         {
           withCredentials: true,
         }
       );
-      return response;
+      return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.log("the error caught by the axios createAFarm thunk");
-        return rejectWithValue(error.response?.data || "something went wrong");
+        console.log("the error caught by the axios createAFarm thunk", error);
+        return rejectWithValue(
+          error.response?.data.message || "something went wrong"
+        );
       }
       return rejectWithValue("unknown error occurred");
     }
