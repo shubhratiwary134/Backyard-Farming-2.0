7c4d4c491be9968ff298c10de6439c598b31f4ca
shubhra tiwary <shubhratiwary5@gmail.com>
2024-12-18 19:01:22 +0530

    slice created

--- Client/Plantorium/src/store/slices/userSlice.ts
+++ Client/Plantorium/src/store/slices/userSlice.ts
@@ -1,3 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const userSlice = createSlice();
+import { checkAndAddUserInTheDBThunk } from "../thunks/userThunk";
+interface initialStateInterface {
+  currentUser: object | null;
+  status: "idle" | "completed" | "failed" | "pending";
+  hasChecked: boolean;
+  error: string | null;
+}
+const initialState: initialStateInterface = {
+  currentUser: null,
+  status: "idle",
+  hasChecked: false,
+  error: null,
+};
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(checkAndAddUserInTheDBThunk.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(checkAndAddUserInTheDBThunk.fulfilled, (state, action) => {
+        state.status = "completed";
+        state.currentUser = action.payload;
+        state.hasChecked = true;
+      })
+      .addCase(checkAndAddUserInTheDBThunk.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = (action.payload as string) || "something went wrong";
+      });
+  },
+});
+export default userSlice.reducer;
