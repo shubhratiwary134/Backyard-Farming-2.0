a17419f5367093420fd4c799bc65c64e4103eb01
shubhra tiwary <shubhratiwary5@gmail.com>
2024-12-29 20:05:51 +0530

    Slice for the Myfarms created

--- Client/Plantorium/src/store/slices/plantoriumSlice.ts
+++ Client/Plantorium/src/store/slices/plantoriumSlice.ts
@@ -1,8 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { createAFarm } from "../thunks/plantoriumThunk";
+import { createAFarm, myFarms } from "../thunks/plantoriumThunk";
 interface Farm {
-  id: string;
-  name: string;
+  userId: string; // Reference to the User
+  averageRainfall: number; // Average rainfall in mm
+  soilType: string; // e.g., Humus, Loamy, Sandy, Alluvial
+  soilColor: string; // e.g., Black, Brownish
+  soilTexture: string; // e.g., Soft, Hard, Airy
+  soilPH: number; // pH value of the soil (0 to 14)
+  pastCrops: string[]; // List of crops grown in the past 3 years
+  cropDiseases: string[]; // List of diseases that have affected crops
+  affectedCrops: string[]; // List of crops affected by diseases
+  waterSupply: string; // e.g., Channel, Tube Well, Other
+  landArea: number; // Area of the land in acres or square meters
+  createdAt?: Date; // Optional, defaults to Date.now
+  Address: string; // Address of the Plantorium
+  photos: string[]; // List of photo URLs
 }
 
 interface plantoriumInitialState {
@@ -28,11 +40,28 @@ const plantoriumSlice = createSlice({
       })
       .addCase(createAFarm.fulfilled, (state, action) => {
         state.status = "completed";
-        state.plantoriums.push(action.payload.plantorium);
+        if (action.payload && action.payload.plantorium) {
+          state.plantoriums.push(action.payload.plantorium);
+        }
       })
       .addCase(createAFarm.rejected, (state, action) => {
         state.status = "failed";
         state.error = (action.payload as string) || "failed to create A Farm";
+      });
+
+    builder
+      .addCase(myFarms.pending, (state) => {
+        state.status = "pending";
+        state.error = null;
+      })
+      .addCase(myFarms.fulfilled, (state, action) => {
+        state.status = "completed";
+        state.plantoriums = Array.isArray(action.payload) ? action.payload : [];
+      })
+      .addCase(myFarms.rejected, (state, action) => {
+        state.status = "failed";
+        state.error =
+          (action.payload as string) || "failed to Fetch Users Farms";
       });
   },
 });
