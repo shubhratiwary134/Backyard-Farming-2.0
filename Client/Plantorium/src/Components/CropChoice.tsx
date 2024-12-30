f87d86b3018bf07fbae66fe96d7ee197a52dea06
shubhra tiwary <shubhratiwary5@gmail.com>
2024-12-30 17:34:35 +0530

    CropChoice component made

--- Client/Plantorium/src/Components/CropChoice.tsx
+++ Client/Plantorium/src/Components/CropChoice.tsx
@@ -1,5 +1,14 @@
+import { useAppSelector } from "../store/Hook";
+
 const CropChoice = () => {
-  return <div>CropChoice</div>;
+  const { cropChoices } = useAppSelector((state) => state.plantorium);
+  return (
+    <div className="flex justify-around items-center">
+      {cropChoices.map((crop) => {
+        return <div>{crop}</div>;
+      })}
+    </div>
+  );
 };
 
 export default CropChoice;
