b63719d3e2d043ded3d165f1e9ff12a05c7a8384
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-15 01:59:46 +0530

    responsive tailwind changes

--- Client/Plantorium/src/Components/BannerImage.tsx
+++ Client/Plantorium/src/Components/BannerImage.tsx
@@ -21,16 +21,16 @@ const BannerImage = () => {
     >
       <img src={marketPic} className="hidden xl:block object-contain w-1/4" />
       <div className="w-1/2 relative flex flex-col gap-10 items-center">
-        <div className="hidden sm:block sm:text-6xl md:text-8xl lg:text-9xl lg:mt-10 text-center text-[#3ba944] font-heading">
+        <div className="hidden sm:block sm:text-6xl md:text-8xl lg:text-9xl lg:mt-10 xl:mt-0 xl:text-8xl 2xl:text-9xl text-center  text-[#3ba944] font-heading">
           Backyard Farming 2.0
         </div>
-        <div className="text-md md:text-xl text-[#296633] text-center font-Montserrat">
+        <div className="text-md md:text-xl xl:text-xl 2xl:text-2xl text-[#296633] text-center font-Montserrat">
           Turn your backyard into a thriving ecosystem with personalized guides.
           Your sustainable journey begins now with Backyard Farming 2.O !!!
         </div>
         <SignedIn>
           <StartFarmButton
-            className="mt-10 text-lg font-dancing lg:text-2xl bg-[#3ba944] hover:bg-[#246d27] duration-100 rounded-tl-full rounded-br-full text-white text-md text-center self-center px-4 py-2 m-2 lg:px-8"
+            className="mt-10 text-lg font-dancing lg:text-2xl xl:text-4xl bg-[#3ba944] hover:bg-[#246d27] duration-100 rounded-tl-full rounded-br-full text-white text-md text-center self-center px-4 py-2 m-2 lg:px-8"
             text="Start Your Farm"
           />
         </SignedIn>
