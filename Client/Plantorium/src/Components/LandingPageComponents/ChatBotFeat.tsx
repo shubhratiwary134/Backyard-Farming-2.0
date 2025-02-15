98d81cab043ef35aed9b1dbf422194d136a5e0ac
shubhra tiwary <shubhratiwary5@gmail.com>
2025-02-15 13:29:19 +0530

    upper half of the team vision prepared

--- Client/Plantorium/src/Components/LandingPageComponents/ChatBotFeat.tsx
+++ Client/Plantorium/src/Components/LandingPageComponents/ChatBotFeat.tsx
@@ -1,5 +1,14 @@
+import chatbotImage from "../../Assests/imageChatbot2.jpg";
 const ChatBotFeat = () => {
-  return <div className="w-screen h-screen "></div>;
+  return (
+    <div className="w-screen h-screen flex gap-40 p-10">
+      <div className="text-4xl font-poppins">
+        Have your <br></br>
+        <span className="text-7xl text-blue-500">Personal Guide</span>
+      </div>
+      <img src={chatbotImage} className="object-contain p-10" />
+    </div>
+  );
 };
 
 export default ChatBotFeat;
--- Client/Plantorium/src/Components/LandingPageComponents/TeamVision.tsx
+++ Client/Plantorium/src/Components/LandingPageComponents/TeamVision.tsx
@@ -1,5 +1,32 @@
 const TeamVision = () => {
-  return <div className="h-screen w-screen bg-black"></div>;
+  return (
+    <div className="h-screen w-screen bg-black text-white ">
+      <div className="flex flex-col p-20">
+        <div className="w-full flex justify-between font-poppins">
+          <div className="flex flex-col items-start">
+            <p className="text-5xl ">
+              The <br></br>
+              <span className="text-8xl text-yellow-300 tracking-widest font-poppins">
+                Creative
+              </span>
+              <br></br>
+            </p>
+            <p className="text-5xl self-end">Team</p>
+          </div>
+          <div className="self-end w-[55%]">
+            <p className="text-center text-xl text-gray-400">
+              At Backyard Farming 2.0, we dream of a future where every backyard
+              transforms into a flourishing, sustainable haven. Our mission is
+              to empower individuals to cultivate their own green spaces by
+              blending the magic of nature with the precision of AI. Together,
+              we're making farming smarter, simpler, and more rewarding.
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default TeamVision;
