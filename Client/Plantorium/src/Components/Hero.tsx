d7350e5e3dd2f59d23182468cf1fb1a375c51c2d
shubhra tiwary <shubhratiwary5@gmail.com>
2025-02-17 10:24:50 +0530

    Hero responsiveness complete

--- Client/Plantorium/src/Components/Hero.tsx
+++ Client/Plantorium/src/Components/Hero.tsx
@@ -9,9 +9,9 @@ import StartFarmButton from "./StartFarmButton";
 import { motion } from "motion/react";
 const BannerImage = () => {
   return (
-    <motion.div className=" w-full flex items-center h-[95%]  gap-5 text-white font-poppins">
+    <motion.div className=" w-full flex items-center h-[75%] lg:h-[95%]  gap-5 text-white font-poppins">
       <div className="w-full relative flex flex-col  items-center ">
-        <div className=" text-7xl p-4 xl:text-[2.8rem] 2xl:text-[3.8rem] tracking-[21px]">
+        <div className="text-7xl lg:text-[2.3rem] pl-10 text-center lg:p-4 xl:text-[2.8rem] 2xl:text-[3.8rem] xl:tracking-[21px] tracking-[16px]">
           FARM FRESH, BACKYARD GROWN
         </div>
         <SignedIn>
@@ -21,13 +21,13 @@ const BannerImage = () => {
           />
         </SignedIn>
         <SignedOut>
-          <div className="mt-20 flex gap-5 items-center tracking-[8px]">
+          <div className="mt-20 flex gap-5 items-center tracking-[3px] xl:tracking-[8px]">
             <SignUpButton>
-              <button className="text-3xl py-2 px-4">Sign Up</button>
+              <button className="text-xl py-2 px-4 xl:text-2xl">Sign Up</button>
             </SignUpButton>
             <hr className="w-[2px] h-[40px] bg-white border-none" />
             <SignInButton>
-              <button className="text-3xl  py-2 px-4 ">Log In</button>
+              <button className="text-xl  py-2 px-4 xl:text-2xl">Log In</button>
             </SignInButton>
           </div>
         </SignedOut>
