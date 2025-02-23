f3f963f8ead8bcabe575923ead81f2599c18f3fa
shubhra tiwary <shubhratiwary5@gmail.com>
2025-02-23 19:04:10 +0530

    changed the plantoriumSlice accordingly for the one-one relationship for plantorium and user , made the initialState empty values for easy access like "plantorium.soilType" and avoid null value checks everywhere Also made initial changes to the report thunk

--- Client/Plantorium/src/store/slices/reportSlice.ts
+++ Client/Plantorium/src/store/slices/reportSlice.ts
@@ -1,16 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 import generateReportThunk from "../thunks/reportThunk";
+interface reportIntialState {
+  reportText: string;
+  status: "idle" | "pending" | "completed" | "failed";
+  error: string | null;
+}
+
+const initialState: reportIntialState = {
+  reportText: "",
+  status: "idle",
+  error: null,
+};
 
 const reportSlice = createSlice({
   name: "Report",
-  initialState: {},
+  initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(generateReportThunk.pending, () => {
       console.log("generate report pending");
     });
-    builder.addCase(generateReportThunk.fulfilled, (_, action) => {
-      console.log(action.payload);
+    builder.addCase(generateReportThunk.fulfilled, (state, action) => {
+      state.status = "completed";
+      state.reportText = action.payload;
     });
     builder.addCase(generateReportThunk.rejected, () => {
       console.log("genrateReportEndpoint Rejected");
