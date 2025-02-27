6e0189c704a0e84b1e81d88092cceba3c21c982f
shubhra tiwary <shubhratiwary5@gmail.com>
2025-02-28 03:57:04 +0530

    added conditions for the fetching of backend report

--- Client/Plantorium/src/Pages/Report.tsx
+++ Client/Plantorium/src/Pages/Report.tsx
@@ -2,18 +2,27 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/Hook";
 import { getReportThunk } from "../store/thunks/reportThunk";
 import { useUser } from "@clerk/clerk-react";
+import LoadingScreen from "../Components/LoadingScreen";
 
 const Report = () => {
   const dispatch = useAppDispatch();
   const { user } = useUser();
-  const { reportText } = useAppSelector((state) => state.report);
+  const { reportText, reportStatus } = useAppSelector((state) => state.report);
   useEffect(() => {
     const userId = user?.id;
-    if (userId) {
+    if (userId && reportStatus === "notGenerated") {
       dispatch(getReportThunk(userId));
     }
-  });
-  return <div>{reportText}</div>;
+  }, [dispatch, user?.id, reportStatus]);
+  return (
+    <div>
+      {reportStatus == "generated" ? (
+        <div>{reportText}</div>
+      ) : (
+        <LoadingScreen />
+      )}
+    </div>
+  );
 };
 
 export default Report;
