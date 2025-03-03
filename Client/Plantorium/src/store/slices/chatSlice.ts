85d291f37d3df9b9c3bf1cb9329d368c32ab740f
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-03 22:07:20 +0530

    chatSlice initial work completed interfaces introduced

--- Client/Plantorium/src/store/slices/chatSlice.ts
+++ Client/Plantorium/src/store/slices/chatSlice.ts
@@ -1,14 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface currentChatInterface {}
+interface Message {
+  role: "user" | "bot";
+  text: string;
+  timeStamp?: string;
+}
+interface currentChatInterface {
+  currentChatId: string;
+  currentMessages: Message[];
+}
 interface chatInterface {
   currentChat: currentChatInterface;
-  chats: Object[];
+  chats: { chatId: string; messages: Message[] }[];
   status: "idle" | "loading" | "completed";
   error: string | null;
 }
 const initialState: chatInterface = {
-  currentChat: null,
+  currentChat: {
+    currentChatId: "",
+    currentMessages: [],
+  },
   chats: [], // to minimize api calls for past chats
   status: "idle",
   error: null,
