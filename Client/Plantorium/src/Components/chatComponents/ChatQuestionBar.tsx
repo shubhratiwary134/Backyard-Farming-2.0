c513805a998dfeb7cbd49ef09e42e2046f08404f
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-06 01:33:46 +0530

    added formik value for the query in the chat , introduced a type for the same and added validation schema

--- Client/Plantorium/src/Components/chatComponents/ChatQuestionBar.tsx
+++ Client/Plantorium/src/Components/chatComponents/ChatQuestionBar.tsx
@@ -1,18 +1,40 @@
+import { Formik } from "formik";
 import { FaLeaf } from "react-icons/fa6";
+import { chatSchema } from "../../Schema/ChatSchema";
+
+import { ChatValues } from "../../Types/ChatTypes";
 const ChatQuestionBar = () => {
-  const handleSubmit = () => {
-    console.log("submitting");
+  const handleSubmit = (values: ChatValues) => {
+    console.log(values.query);
+  };
+
+  const initialValues: ChatValues = {
+    query: "",
   };
   return (
-    <div className=" w-full mb-10 flex gap-5">
-      <input
-        type="text"
-        className="w-4/5 h-5 py-8 px-10 bg-slate-300 rounded-3xl placeholder:text-zinc-700 placeholder:text-xl focus:outline-none"
-        placeholder="Ask Question "
-      />
-      <button type="submit">
-        <FaLeaf size={48} onClick={handleSubmit} />
-      </button>
+    <div className=" w-full mb-10 ">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={chatSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form className="w-full flex gap-5 " onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="query"
+              value={values.query}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="w-4/5 h-5 py-8 px-10 bg-slate-300 rounded-3xl placeholder:text-zinc-700 placeholder:text-xl focus:outline-none"
+              placeholder="Ask Question "
+            />
+            <button type="submit">
+              <FaLeaf size={48} />
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
---
+++ Client/Plantorium/src/Types/ChatTypes.ts
@@ -0,0 +1,3 @@
+export type ChatValues = {
+  query: string;
+};
