6d5df9643ca92be9c7acbf6b73e398d3f2d6dd10
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-06 00:34:07 +0530

    handle submit function created

--- Client/Plantorium/src/Components/chatComponents/ChatQuestionBar.tsx
+++ Client/Plantorium/src/Components/chatComponents/ChatQuestionBar.tsx
@@ -1,11 +1,18 @@
+import { FaLeaf } from "react-icons/fa6";
 const ChatQuestionBar = () => {
+  const handleSubmit = () => {
+    console.log("submitting");
+  };
   return (
-    <div className=" w-full mb-10 ">
+    <div className=" w-full mb-10 flex gap-5">
       <input
         type="text"
         className="w-4/5 h-5 py-8 px-10 bg-slate-300 rounded-3xl placeholder:text-zinc-700 placeholder:text-xl focus:outline-none"
         placeholder="Ask Question "
       />
+      <button type="submit">
+        <FaLeaf size={48} onClick={handleSubmit} />
+      </button>
     </div>
   );
 };
