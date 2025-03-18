5294d68ff1f46163d29ad6fb64252b6a9d88ce7b
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-18 22:51:34 +0530

    basic layout of new design implemented, removed logic of signedIn from Hero.tsx

--- Client/Plantorium/src/Components/Hero.tsx
+++ Client/Plantorium/src/Components/Hero.tsx
@@ -1,11 +1,5 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-} from "@clerk/clerk-react";
+import { SignInButton, SignUpButton } from "@clerk/clerk-react";
 
-import StartFarmButton from "./StartFarmButton";
 import { motion } from "motion/react";
 const BannerImage = () => {
   return (
@@ -14,23 +8,16 @@ const BannerImage = () => {
         <div className=" text-7xl lg:text-[2.3rem] pl-10 text-center lg:p-4 xl:text-[2.8rem] 2xl:text-[3.8rem] xl:tracking-[18px] tracking-[16px]">
           FARM FRESH, BACKYARD GROWN
         </div>
-        <SignedIn>
-          <StartFarmButton
-            className="mt-20 text-xl py-2 px-4 xl:text-2xl tracking-[3px] border-x-2  border-white"
-            text="Start Your Farm"
-          />
-        </SignedIn>
-        <SignedOut>
-          <div className="mt-20 flex gap-5 items-center tracking-[3px] xl:tracking-[8px]">
-            <SignUpButton>
-              <button className="text-xl py-2 px-4 xl:text-2xl">Sign Up</button>
-            </SignUpButton>
-            <hr className="w-[2px] h-[40px] bg-white border-none" />
-            <SignInButton>
-              <button className="text-xl  py-2 px-4 xl:text-2xl">Log In</button>
-            </SignInButton>
-          </div>
-        </SignedOut>
+
+        <div className="mt-20 flex gap-5 items-center tracking-[3px] xl:tracking-[8px]">
+          <SignUpButton>
+            <button className="text-xl py-2 px-4 xl:text-2xl">Sign Up</button>
+          </SignUpButton>
+          <hr className="w-[2px] h-[40px] bg-white border-none" />
+          <SignInButton>
+            <button className="text-xl  py-2 px-4 xl:text-2xl">Log In</button>
+          </SignInButton>
+        </div>
       </div>
     </motion.div>
   );
--- Client/Plantorium/src/Pages/HomePage.tsx
+++ Client/Plantorium/src/Pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import homePageImage from "../Assests/BannerImage-DarkTheme.jpg";
 const HomePage = () => {
-  return <></>;
+  return (
+    <div className="flex justify-between ">
+      <div></div>
+      <img src={homePageImage} className="w-1/3 object-cover"></img>
+    </div>
+  );
 };
 
 export default HomePage;
