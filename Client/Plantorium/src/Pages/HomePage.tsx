45c07d8be41e7d9494b233f7b45ab06a637af59c
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-18 23:03:05 +0530

    design update 2

--- Client/Plantorium/src/Pages/HomePage.tsx
+++ Client/Plantorium/src/Pages/HomePage.tsx
@@ -1,9 +1,14 @@
-import homePageImage from "../Assests/BannerImage-DarkTheme.jpg";
+import homePageImage from "../Assests/BannerImage-lightTheme.png";
 const HomePage = () => {
   return (
-    <div className="flex justify-between ">
-      <div></div>
-      <img src={homePageImage} className="w-1/3 object-cover"></img>
+    <div className="flex justify-between px-10 ">
+      <div className="w-1/2 flex flex-col gap-20 mt-20">
+        <div className="text-7xl font-poppins text-center">
+          Backyard Farming 2.O
+        </div>
+        <div className="text-3xl font-poppins text-center"></div>
+      </div>
+      <img src={homePageImage} className="w-[30%] object-cover" />
     </div>
   );
 };
