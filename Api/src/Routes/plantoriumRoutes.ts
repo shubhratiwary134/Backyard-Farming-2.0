b9c9efb59084787edad2b68a017cedb2ffe47693
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-28 01:58:15 +0530

    Refactored code , irrelevant controller

--- Api/src/Routes/plantoriumRoutes.ts
+++ Api/src/Routes/plantoriumRoutes.ts
@@ -3,7 +3,6 @@ import {
   createPlantorium,
   deleteSpecificPlantorium,
   getReport,
-  getSpecificPlantorium,
   postGeneratedReport,
   updateSpecificPlantorium,
 } from "../Controllers/PlantoriumController";
@@ -16,7 +15,6 @@ plantoriumRoutes.post(
   upload.array("Photos", 4),
   createPlantorium
 );
-plantoriumRoutes.get("/:id", getSpecificPlantorium);
 plantoriumRoutes.delete("/:id", deleteSpecificPlantorium);
 plantoriumRoutes.put("/:id", updateSpecificPlantorium);
 plantoriumRoutes.post("/generateReport", postGeneratedReport);
