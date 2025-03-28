199b8bc5ccbd37ecd9e6e049529475585d7339b3
shubhra tiwary <shubhratiwary5@gmail.com>
2025-03-29 01:03:59 +0530

    refactored code

--- Client/Plantorium/src/Pages/MyFarm.tsx
+++ Client/Plantorium/src/Pages/MyFarm.tsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from "react";
 import Spline from "@splinetool/react-spline";
-import { myFarm } from "../store/thunks/plantoriumThunk";
-import { useUser } from "@clerk/clerk-react";
-import { useAppDispatch, useAppSelector } from "../store/Hook";
 
-const MyFarm: React.FC = () => {
-  const { user } = useUser();
-  const dispatch = useAppDispatch();
-  const { averageRainfall, waterSupply, soilType } = useAppSelector(
-    (state) => state.plantorium.plantorium
-  );
-  useEffect(() => {
-    const userId = user?.id;
-    if (userId) {
-      dispatch(myFarm(userId));
-      console.log(averageRainfall, waterSupply, soilType);
-    }
-  }, [user, dispatch, averageRainfall, waterSupply, soilType]);
-  // will be using 2 useEffects one for the getting the data on the Mount , rest when the spline scene loads
+const MyFarm = () => {
   return (
-    <Spline scene="https://prod.spline.design/FeRzpsDxlT20lDyK/scene.splinecode" />
+    <div>
+      <Spline scene="https://prod.spline.design/FeRzpsDxlT20lDyK/scene.splinecode" />
+    </div>
   );
 };
 
