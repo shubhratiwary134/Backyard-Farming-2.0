2b2b67e63a31369034088692a01cdd123ed4f37b
shubhra tiwary <shubhratiwary5@gmail.com>
2025-05-10 16:35:06 +0530

    spline scene activated

--- Client/Plantorium/src/Pages/MyFarm.tsx
+++ Client/Plantorium/src/Pages/MyFarm.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 const MyFarm = () => {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/FeRzpsDxlT20lDyK/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/FeRzpsDxlT20lDyK/scene.splinecode"/>
     </div>
   );
 };
